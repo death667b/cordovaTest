8afaa67547244da88251948281df0b85fa8be6ec
death667b <idaniel@spinnys.com>
2017-08-05 19:17:46 +1000

    Pushing the localStorage set and get again? again?

--- platforms/osx/www/js/index.js
+++ platforms/osx/www/js/index.js
@@ -20,6 +20,13 @@ var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        document.getElementById("setLocalStorage").addEventListener("click", setLocalStorage); 
+        document.getElementById("showLocalStorage").addEventListener("click", showLocalStorage); 
+        document.getElementById("removeProjectFromLocalStorage").addEventListener 
+        ("click", removeProjectFromLocalStorage); 
+        document.getElementById("getLocalStorageByKey").addEventListener 
+        ("click", getLocalStorageByKey);  
+        var localStorage = window.localStorage; 
     },
 
     // deviceready Event Handler
@@ -44,3 +51,15 @@ var app = {
 };
 
 app.initialize();
+
+function setLocalStorage() { 
+   localStorage.setItem("Name", "John"); 
+   localStorage.setItem("Job", "Developer"); 
+   localStorage.setItem("Project", "Cordova Project"); 
+} 
+
+function showLocalStorage() { 
+   console.log(localStorage.getItem("Name")); 
+   console.log(localStorage.getItem("Job")); 
+   console.log(localStorage.getItem("Project")); 
+} 
